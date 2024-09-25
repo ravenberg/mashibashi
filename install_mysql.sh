e33f677b97c9e5e160afa92704e1ef9f242a9234
Lee Ravenberg <lee@ravenberg.dev>
2024-09-25 22:49:11 +0200

    adding service to systemctl

--- install_mysql.sh
+++ install_mysql.sh
@@ -5,3 +5,8 @@ sudo apt update
 
 echo "Installing mysql-server"
 sudo apt install mysql-server -y
+
+echo "Adding service to systemctl"
+sudo systemctl enable mysqld.service
+sudo systemctl start mysqld.service
+systemctl status mysqld.service
