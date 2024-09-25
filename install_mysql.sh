360d2e978c9deb13b92d3d33bbbcad5c65e8c11a
Lee Ravenberg <lee@ravenberg.dev>
2024-09-25 22:33:44 +0200

    using apt instead

--- install_mysql.sh
+++ install_mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Download the MySQL APT repository config tool
-echo "Installing MySQL APT repository..."
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.32-1_all.deb
+echo "Updating repostories"
+apt update
+apt install mysql-server
